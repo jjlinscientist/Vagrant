c63d0fd2b90024a0f7b463b6d017ae9fd9610f55
Ubuntu <ubuntu@ip-10-0-0-179.ec2.internal>
2018-12-27 02:36:25 +0000

    updated config_nvim/ directory

--- provisions/bootstrap2.sh
+++ provisions/bootstrap2.sh
@@ -3,22 +3,27 @@
 # install neovim, optional dependencies
 # install other packages
 pacman -S --noconfirm neovim python-neovim xclip
-pacman -S --noconfirm docker git tmux parallel tree
+pacman -S --noconfirm docker git tmux parallel tree #gcc-libs
+
+# create github directory
+cd
+mkdir -p $HOME/git
 
 # pull bash config from my github
-cd
-mkdir -p ~/.config/bash
-git clone https://github.com/jjlinscientist/bash_profile ~/.config/bash
-ln -sf ~/.config/bash/.bash* ~/
-source ~/.bashrc
+cd $HOME/git
+git clone https://github.com/jjlinscientist/bash_profile
+ln -sf $HOME/git/bash_profile/.bash* $HOME/
+source $HOME/.bashrc
 
-# install Vundle and pull neovim config from my github
-cd
-mkdir -p ~/.config/nvim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
-git clone https://github.com/jjlinscientist/nvim ~/.config/nvim/
+# install vim-plug and pull neovim config from my github
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cd $HOME/git
+git clone https://github.com/jjlinscientist/config_nvim
+ln -sf $HOME/git/config_nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sf $HOME/git/config_nvim/vimrc.d $HOME/.config/nvim/vimrc.d
 
 # pull tmux config from my github
-cd
-git clone https://github.com/jjlinscientist/tmux_config ~/.config/tmux
-ln -sf ~/.config/tmux/.tmux.conf ~/
+cd $HOME/git
+git clone https://github.com/jjlinscientist/tmux_config
+ln -sf $HOME/git/tmux_config/.tmux.conf $HOME/
