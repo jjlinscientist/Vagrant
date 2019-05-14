fffca031301f06133c7f82710a70512ef3f4ad69
Ubuntu <ubuntu@ip-10-1-1-220.ec2.internal>
2019-05-14 04:02:14 +0000

    updated neovim install

--- provisions/ubuntu_bootstrap.sh
+++ provisions/ubuntu_bootstrap.sh
@@ -7,42 +7,46 @@ sudo apt-get -y upgrade
 
 # install neovim, optional dependencies
 sudo apt-get -y install python-dev python-pip python3-dev python3-pip
-sudo apt-get -y install neovim xclip tmux parallel tree 
+sudo apt-get -y install xclip tmux parallel tree wget
+mkdir -p $HOME/exec/appimage
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O $HOME/exec/appimage/nvim.appimage
+.$HOME/exec/appimage/nvim.appimage --appimage-extract
+ln -s $HOME/exec/nvim/squashfs-root/usr/bin/nvim $HOME/exec/nvim
 
 # create github directory
 cd
-mkdir -p /home/ubuntu/git
+mkdir -p $HOME/git
 
 # pull bash config from my github
-cd /home/ubuntu/git
+cd $HOME/git
 rm -rf bash_profile
 git clone https://github.com/jjlinscientist/bash_profile
-ln -sf /home/ubuntu/git/bash_profile/.bash* /home/ubuntu/
-source /home/ubuntu/.bashrc
+ln -sf $HOME/git/bash_profile/.bash* $HOME/
+source $HOME/.bashrc
 
 # install vim-plug and pull neovim config from my github
-curl -fLo /home/ubuntu/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cd /home/ubuntu/git
+cd $HOME/git
 rm -rf config_nvim
 git clone https://github.com/jjlinscientist/config_nvim
-mkdir -p /home/ubuntu/.config/nvim/plugged
-ln -sf /home/ubuntu/git/config_nvim/init.vim /home/ubuntu/.config/nvim/
-ln -sf /home/ubuntu/git/config_nvim/vimrc.d /home/ubuntu/.config/nvim/
+mkdir -p $HOME/.config/nvim/plugged
+ln -sf $HOME/git/config_nvim/init.vim $HOME/.config/nvim/
+ln -sf $HOME/git/config_nvim/vimrc.d $HOME/.config/nvim/
 
 # pull tmux config from my github
-cd /home/ubuntu/git
+cd $HOME/git
 rm -rf tmux_config
 git clone https://github.com/jjlinscientist/tmux_config
-ln -sf /home/ubuntu/git/tmux_config/.tmux.conf /home/ubuntu/
+ln -sf $HOME/git/tmux_config/.tmux.conf $HOME/
 
 # run nvim with :PlugInstall
 
 nvim +PlugInstall +qall > /dev/null
 nvim +UpdateRemotePlugins +qall > /dev/null
 
-# change ownership of /home/ubuntu recursively
-# cd /home/ubuntu
+# change ownership of $HOME recursively
+# cd $HOME
 # chown -hR ubuntu:ubuntu .
 
 # install other packages
