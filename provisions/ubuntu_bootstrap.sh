aadabe5fe316876eeefdd66fa88fb51a2fca64c1
Ubuntu <ubuntu@ip-10-1-9-45.ec2.internal>
2019-05-15 18:41:31 +0000

    updated nvim install

--- provisions/ubuntu_bootstrap.sh
+++ provisions/ubuntu_bootstrap.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 # perpare environment for neovim install
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt-get -y update
 sudo apt-get -y upgrade
+sudo apt-get -y install \
+  software-properties-common \
+  python-dev \
+  python-pip \
+  python3-dev \
+  python3-pip \
+  xclip \
+  tmux \
+  parallel \
+  tree \
+  wget
 
 # install neovim, optional dependencies
-sudo apt-get -y install python-dev python-pip python3-dev python3-pip
-sudo apt-get -y install xclip tmux parallel tree wget
-mkdir -p $HOME/exec/appimage
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O $HOME/exec/appimage/nvim.appimage
-.$HOME/exec/appimage/nvim.appimage --appimage-extract
-ln -s $HOME/exec/nvim/squashfs-root/usr/bin/nvim $HOME/exec/nvim
+mkdir -p $HOME/appimages/nvim/
+mkdir -p $HOME/exec/
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O $HOME/appimages/nvim.appimage
+chmod a+x $HOME/appimages/nvim.appimage
+$HOME/appimages/nvim.appimage --appimage-extract
+mv squashfs-root $HOME/appimages/nvim/
+ln -sf $HOME/appimages/nvim/squashfs-root/usr/bin/nvim $HOME/exec/
+pip3 install neovim
 
 # create github directory
 cd
@@ -51,10 +62,10 @@ nvim +UpdateRemotePlugins +qall > /dev/null
 
 # install other packages
 sudo apt install -y \
+  docker.io \
+  awscli
   # r-base \
   # r-base-dev \
   # libssl-dev \
   # libcurl4-openssl-dev \
   # libxml2-dev \
-  docker.io \
-  awscli
