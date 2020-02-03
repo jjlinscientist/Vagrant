f0679335276896de1c51779d079921eebe7c4eb6
Ubuntu <ubuntu@ip-10-1-10-53.ec2.internal>
2020-02-03 22:52:41 +0000

    changed shebang from usr/bin/env bash to /bin/bash

--- provisions/00_ubuntu_bootstrap.sh
+++ provisions/00_ubuntu_bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # perpare environment for neovim install
 sudo apt-get -y update
--- provisions/01_nvim_bootstrap.sh
+++ provisions/01_nvim_bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # install neovim, optional dependencies
 mkdir -p $HOME/appimages/nvim/
