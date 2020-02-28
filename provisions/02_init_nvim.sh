abef3c65739c65b986ff2022a39806fe50261965
jjlinscientist <jjlin@zr-res-166.ZymoResearch.local>
2020-02-27 21:20:07 -0800

    sourced .bashrc at the beginning of 02_init_nvim.sh

--- provisions/02_init_nvim.sh
+++ provisions/02_init_nvim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+source $HOME/.bashrc
 $HOME/appimages/nvim/squashfs-root/usr/bin/nvim +PlugInstall +qall >> /dev/null
 $HOME/appimages/nvim/squashfs-root/usr/bin/nvim +UpdateRemotePlugins +qall >> /dev/null
